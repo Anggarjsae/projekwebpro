62e50eae42a14a46dd53cd9990d3c788e169ee73
Anggarjsae <64628559+Anggarjsae@users.noreply.github.com>
2021-11-05 08:13:40 +0700

    11/05/2021
    jadinya buat apa nih? ini cuma gabut wkwkw

---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Selamat Datang di Portal Wisata Jawa Tengah</title>
+</head>
+
+<body class="container-fluid p-0">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    
+    <div >
+        <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+            <a class="navbar-brand" href="#">Portal Wisata Jateng</a>
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Paket Wisata</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Promo</a>
+                    </li>
+                    <?php
+                    session_start();
+                    ?>
+                    <?php if(isset($_SESSION['is_login'])): ?>
+                    <li class="nav-item">
+                            <a class="nav-link" href="?page=logout">Logout</a>
+                    </li>
+
+                    <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=login">Login</a>
+                    </li>
+
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </nav>
+        <?php 
+
+            define('Gelang', 1);
+
+            require_once('koneksi.php');
+
+            if(isset($_GET['page']))
+            {
+                $page = $_GET['page'];
+                
+                if(file_exists($page.".php"))
+                {
+                     // tampilkan halaman yang diminta
+                     require_once $page.".php";       
+                }
+                else
+                {
+                    die("Mau kemana?");
+                    require_once "404.php";
+                }
+            }
+            else
+            {
+                // tampilkan halaman default
+                require_once "beranda.php";
+            }
+        ?>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+</body>
+
+</html>
